7804aef8dd413d687abe910c739046038ed6c5f8
theamit-009 <aim.amit9@gmail.com>
2019-08-19 18:46:27 +0530

    connectflashAndexpress-session

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var {Pool,Client} = require('pg');
 var passport = require('passport');
 var dotenv = require('dotenv');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
